da74226637a562e0f24d450b0b3fdb8caa3506d0
Ajay <ajayas4@gmail.com>
2018-10-28 17:33:44 +0530

    deleted

--- watercan/app.js
+++
@@ -1,9 +0,0 @@
-var express=require("express");
-var app=express();
-app.listen(3000,()=>{
-    console.log("server created successfully");
-})
-
-app.get("/getUser",(req,res,next)=>{
-    res.json({'name' : 'ajay'});
-})
