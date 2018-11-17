f7ac11b0d2655711acf33a3f8ccdd7599e66c9b9
Ajay <ajayas4@gmail.com>
2018-11-17 15:58:26 +0530

    sprinboot initial commit
    sprinboot initial commit

---
+++ wc/src/main/java/com/dream/wc/WcApplication.java
@@ -0,0 +1,12 @@
+package com.dream.wc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcApplication.class, args);
+	}
+}
